54038af822218982a7e675bd115dc385ed804fc4
maxime digne <digne@lan.sio.lyceefulbert.fr>
2026-01-16 16:53:52 +0100

    debut

---
+++ index.php
@@ -0,0 +1,26 @@
+<title>Jeu de la vie</title>
+<link rel="stylesheet" href="style.css">
+
+<?php
+echo "<body>";
+
+/* ÉCRAN D’ACCUEIL AVANT LE JEU */
+if (!isset($_GET["run"])): ?>
+    <div id="accueil">
+        <h1 class="debutjeux">Jeu de la vie, règles du jeu</h1>
+        <ul>
+            Vous entrez dans le jeu de votre vie. <br>
+            Vous allez faire des choix qui vont décider de comment vous allez finir, heureux, en dépréssion, ultra riche.<br>
+            C'est a vous de faire vos choix pour définir comment vous allez vivre.<br>
+        </ul>
+        <button id="btnStart">Lancer le jeu</button>
+    </div>
+
+    <script>
+        document.getElementById("btnStart").addEventListener("click", function () {
+            window.location.href = "?run=1";
+        });
+    </script>
+<?php
+    exit;
+endif;
